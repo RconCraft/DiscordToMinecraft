bc53fefffd6aa7688692aee3fe4808e833bc1dad
MCPlayingTV <63210893+MCPlayingTV@users.noreply.github.com>
2020-10-12 01:22:51 -0700

    Add files via upload

---
+++ src/main.php
@@ -0,0 +1,27 @@
+
+declare(strict_types=1);
+
+namespace RconCraft;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class MainClass extends PluginBase{
+
+	public function onLoad() : void{
+		$this->getLogger()->info(TextFormat::WHITE . "RconCraft is ready!");
+	}
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		switch($command->getName()){
+			case "rcsay":
+				$sender->sendMessage("[Discord] " . $args[0] . " > " . $args[1]);
+
+				return true;
+			default:
+				return false;
+		}
+	}
+}
